7ebccc96a78f824cb8c1b9c98142b998c2f22056
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-08-09 22:03:35 +0100

    add header, form and function to add new ingredients

---
+++ App.jsx
@@ -0,0 +1,23 @@
+/**
+ * Challenge: Build the Header component in a separate file
+ * and render it here in the App component
+ */
+
+import Header from "./components/Header";
+import React from "react";
+import Main from "./components/Main";
+
+export default function App() {
+
+  const test = React.useState("test")
+
+  console.log(test);
+
+  return (
+    <>
+    <Header />
+   <Main />
+    </>
+   
+  )
+}
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+           <header>
+        <img src="./images/chef-claude-icon.png" alt="logo of a chef" />
+        <h1>Chef Claude</h1>
+      
+      </header>
+    )
+}
